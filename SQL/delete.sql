128a5b944c6ed3d9a9e85ef2a40f81bb1f722609
vietvince <vincentttran03@gmail.com>
2024-11-16 13:52:23 -0500

    added .sql files

---
+++ SQL/delete.sql
@@ -0,0 +1,29 @@
+-- DELETE DEPARTMENT
+CREATE OR REPLACE FUNCTION delete_department(
+    p_department_ID INT
+)
+RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM Department
+    WHERE department_ID = p_department_ID;
+END;
+$$ LANGUAGE plpgsql;
+
+-- EXAMPLE FUNCTION CALL
+SELECT delete_department(1);
+
+-- DELETE MAJOR
+CREATE OR REPLACE FUNCTION delete_major(
+    p_major_title VARCHAR(100)
+)
+RETURNS VOID AS
+$$
+BEGIN
+    DELETE FROM Major
+    WHERE major_title = p_major_title;
+END;
+$$ LANGUAGE plpgsql;
+
+-- EXAMPLE FUNCTION CALL
+SELECT delete_major('Computer Science');
