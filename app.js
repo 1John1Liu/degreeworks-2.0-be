ae7de66e56385397a067806779ee1206551b5839
vietvince <vincentttran03@gmail.com>
2024-11-16 17:28:36 -0500

    created create-department function

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ const session = require('express-session');
 const sessionConfig = require('./config/session');
 const authRoutes = require('./routes/auth');
 const dashboardRoutes = require('./routes/dashboard');
+const createDepartmentRoutes = require('./routes/create-department')
 const { attachUser } = require('./middleware/auth');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(session(sessionConfig));
 app.use(attachUser); // Attaches user from session to res.locals
 
 // Routes
 app.use('/', authRoutes);
 app.use('/dashboard', dashboardRoutes);
+app.use('/create-department', createDepartmentRoutes);
 
 // Server
 const PORT = process.env.PORT || 3000;
